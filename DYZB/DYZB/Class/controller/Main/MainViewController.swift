b605a528a6429e3b34864c4600ea6e520bde1e66
gaoyonghua <13701747745@163.com>
2016-09-19 12:17:15 +0800

    项目框架

---
+++ DYZB/DYZB/Class/controller/Main/MainViewController.swift
@@ -0,0 +1,41 @@
+
+//
+//  MainViewController.swift
+//  DYZB
+//
+//  Created by 高勇华 on 16/9/19.
+//  Copyright © 2016年 huading. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+     addChildVC("Home")
+     addChildVC("Live")
+     addChildVC("Follow")
+     addChildVC("Perfile")
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    private func addChildVC(StoryboardName:String){
+        
+        let childVC = UIStoryboard(name: StoryboardName, bundle: nil).instantiateInitialViewController()!
+        addChildViewController(childVC)
+     
+    }
+    
+    
+    
+    
+    
+    
+   }
